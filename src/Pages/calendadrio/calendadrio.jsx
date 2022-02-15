86cf1c890d2f08e0a44c85483d479b8cb3afcf0d
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-02-15 15:49:41 -0300

    redirect para ficha

--- src/Pages/calendadrio/calendadrio.jsx
+++ src/Pages/calendadrio/calendadrio.jsx
@@ -58,12 +58,8 @@ export default function Calendario() {
           var minutoFim = ndt.getMinutes();
           let vld = item.validacao === "VIDEO CONF" ? "VC" : item.validacao === "INTERNA" ? "INT" : item.validacao === "EXTERNA" ? "EXT" : item.validacao === "VIDEO INTERNA" ? "VI" : item.validacao === "RENOV ONLINE" ? "RO" : "";
           let identifica = item.validacao === "VIDEO CONF" ? item.validacao : item.validacao === "INTERNA" ? item.validacao : item.validacao === "EXTERNA" ? item.validacao : item.validacao === "VIDEO INTERNA" ? item.validacao : item.validacao === "RENOV ONLINE" ? item.validacao : item.andamento;
-         
           var titulo = vld + "  " + item.id + "  " + item.titulo + " " + item.obs_agenda; 
                
-          
-
-
           return {
                title: titulo,
                id: item.id,
@@ -92,6 +88,7 @@ export default function Calendario() {
                     startAccessor={itensAgenda.start}
                     endAccessor={itensAgenda.end}
                     style={{ height: "90vh", margin: "50px", marginTop: "0" }}
+                    onSelectEvent={(event) => window.open("https://redebrasilrp.com.br/fcw2/abrir_ficha.php?fc="+ event.id) }
                     eventPropGetter={(event, start, end, isSelected) => ({
                          event,
                          start,
@@ -100,8 +97,8 @@ export default function Calendario() {
                          style: {
                               backgroundColor: event.type === "VIDEO CONF" ? "#8e24aa" : event.type === "EXTERNA" ? "#dff800" : event.type === "INTERNA" ? "#F55600" : event.type === "VIDEO INTERNA" ? "#026c1c" : event.type === "RENOV ONLINE" ? "#696969" : event.type === "RENOV ONLINE" ? "#696969" : event.type === "APROVADO" ? "#4c2bf1" : event.type === "EMITIDO" ? "#4c2bf1" : event.type === "INFORMACOES" ? "#fd2600" : "#ffff",
                               
-                              border: event.type === " " ? "3px solid #000000" : "none",
-                              border: "1px solid",
+                              border: event.type === " " ? "3px solid #000000" : "1px solid",
+                    
                               borderColor: event.type === "VIDEO CONF" ? "#471255" : event.type === "EXTERNA" ? "#707C00" : event.type === "INTERNA" ? "#7B2B00" : event.type === "VIDEO INTERNA" ? "#01360E" : event.type === "RENOV ONLINE" ? "#353535" : event.type === "APROVADO" ? "#261679" : event.type === "EMITIDO" ? "#261679" : "#000",
                               
                               color: event.type === "VIDEO CONF" ? "#fff" : event.type === "EXTERNA" ? "#000" : event.type === "INTERNA" ? "#ffff" : event.type === "VIDEO INTERNA" ? "#ffff" : event.type === "RENOV ONLINE" ? "#ffff" : event.type === "APROVADO" ? "#ffff" : event.type === "EMITIDO" ? "#ffff" : event.type === "INFORMACOES" ? "#ffff" : "#000",
